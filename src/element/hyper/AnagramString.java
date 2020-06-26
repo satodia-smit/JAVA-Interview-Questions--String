8a7a1a3234c6d1eb245d9b9695f57303e3484a8b
Hyper-Element <satodiasmit@gmail.com>
2020-06-26 16:28:36 +0530

    Anagaram string

--- src/element/hyper/AnagramString.java
+++ src/element/hyper/AnagramString.java
@@ -1,4 +1,25 @@
 package element.hyper;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class AnagramString {
+    static void checkAnaGram(String aFirst, String aSecond) {
+        String mFirst = aFirst.toLowerCase().replace(" ", "");
+        String mSecond = aSecond.toLowerCase().replace(" ", "");
+
+        char[] aFirstArray = mFirst.toCharArray();
+        char[] aSecondArray = mSecond.toCharArray();
+
+        Arrays.sort(aFirstArray);
+        Arrays.sort(aSecondArray);
+
+        boolean equals = Arrays.equals(aFirstArray, aSecondArray);
+        if (equals) {
+            System.out.println("They are anagram");
+        } else {
+            System.out.println("They are not anagram");
+        }
+
+    }
 }
--- src/element/hyper/Main.java
+++ src/element/hyper/Main.java
@@ -3,11 +3,15 @@ package element.hyper;
 public class Main {
 
     public static void main(String[] args) {
-        testReturnString();
+        testAnagramString();
     }
 
     static void testReturnString() {
         String mString = "This is me!";
         ReverseString.returnString(mString);
+    }
+
+    static void testAnagramString(){
+        AnagramString.checkAnaGram("smit","Mits");
     }
 }
